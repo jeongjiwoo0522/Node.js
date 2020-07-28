707693bf07299ade23d981bd80d1f6cd1475103b
jeongjiwoo0522 <jiwoourty@gmail.com>
2020-07-28 17:33:26 +0900

    first commit

--- modules.js
+++
@@ -1,9 +0,0 @@
-module.exports.parseCookies = (cookie = '') => 
-    cookie
-    .split(';')
-    .map(v => v.split('='))
-    .map(([k, ...vs]) => [k, vs.join('=')])
-    .reduce((acc, [k, v]) => {
-        acc[k.trim()] = decodeURIComponent(v);
-        return acc;
-    }, {});
